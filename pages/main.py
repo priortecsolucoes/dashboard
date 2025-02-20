b143c9ad05e624b6621105ffe4be641059aade14
priortecsolucoes <priortecsolucoes@gmail.com>
2025-02-20 14:11:17 -0300

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -16,7 +16,7 @@ st.set_page_config(page_title="Portal IMND", layout="wide")
 st.markdown("""
     <style>
         section[data-testid="stSidebar"] {display: none;}
-        data-testid="stHeader"], .stAppHeader ;.st-emotion-cache-h4xjwg;.e10jh26i0 {
+        data-testid="stHeader"], .stAppHeader ,.st-emotion-cache-h4xjwg,.e10jh26i0 {
             display: none !important;
         }
         div.block-container {padding-top: 0px !important;}
