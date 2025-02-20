71317ad4d92986755449a0353be63c827b6caaa6
priortecsolucoes <priortecsolucoes@gmail.com>
2025-02-20 13:52:55 -0300

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -16,7 +16,7 @@ st.set_page_config(page_title="Portal IMND", layout="wide")
 st.markdown("""
     <style>
         section[data-testid="stSidebar"] {display: none;}
-        div.block-container {padding-top: 50px !important;}
+        div.block-container {padding-top: 30px !important;}
     
     </style>
 """, unsafe_allow_html=True)
