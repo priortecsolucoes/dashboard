c087f31456589d7ba33a7957071440b3e51ba7ac
priortecsolucoes <priortecsolucoes@gmail.com>
2025-02-21 09:01:54 -0300

    Update amgdash.py

--- pages/amgdash.py
+++ pages/amgdash.py
@@ -6,14 +6,15 @@ from io import BytesIO
 # Configuração do modo escuro
 st.set_page_config(page_title="Portal AMG", layout="wide")
 pagesAcess = st.session_state.get("pagesAcess")
+access = pagesAcess[0]
 st.markdown("""
     <style>
-        div.block-container {padding-top: 30px !important;}
+        div.block-container {padding-top: 16px !important;}
         }
     </style>
     """, unsafe_allow_html=True)
 
-if pagesAcess != 'admin':
+if 'admin' not in access:
     st.markdown("""
     <style>
         section[data-testid="stSidebar"] {display: none;}
